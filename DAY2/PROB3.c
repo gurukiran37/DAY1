0c1534b5d9f230e243c6475cc6455d73f29effbd
gurukiran37 <gurunaik3975@gmail.com>
2025-02-18 23:12:20 +0530

    PROB3.c

--- DAY2/PROB3.c
+++ DAY2/PROB3.c
@@ -1,52 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-void main() 
+int factorial(int n) 
 {
-    int paymentmethod;
-    char cardnum[17]; 
-    char upi[50];     
-    int bank;
-    printf("Select payment method (1-3):\n");
-    printf("1.credit/debit Card\n");
-    printf("2.UPI\n");
-    printf("3.Net banking\n");
-    scanf("%d",&paymentmethod);
-    switch (paymentmethod) 
+    if (n == 0 || n == 1) 
     {
-        case 1:
-            printf("Enter your 16-digit card number: ");
-            scanf("%s", cardnum);
-            if (strlen(cardnum) == 16) 
-            {
-                printf("Payment Successful\n");
-            } else {
-                printf("Transaction Failed. Please try again.\n");
-            }
-            break;
-
-        case 2: 
-            printf("Enter your UPI ID: ");
-            scanf("%s",upi);
-            printf("Payment Successful!\n");
-            break;
-
-        case 3: 
-            printf("Select your bank:\n");
-            printf("1. SBI\n");
-            printf("2. HDFC\n");
-            printf("3. ICICI\n");
-            printf("Enter your choice: ");
-            scanf("%d", &bank);
-            if (bank>= 1 && bank<= 3)
-            {
-                printf("Payment successful!\n");
-            } else {
-                printf("Transaction failed please try again.\n");
-            }
-            break;
-
-        default: 
-            printf("Invalid payment method please try again.\n");
-            break;
+        return 1;
+    } else {
+        return n * factorial(n - 1); 
     }
+}
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num < 0) 
+    {
+        printf("Factorial is not defined for negative numbers.\n");
+    } 
+    else {
+     int result = factorial(num);
+     printf("Factorial of %d is %d\n", num,result);
+    }
+return 0;
 }
