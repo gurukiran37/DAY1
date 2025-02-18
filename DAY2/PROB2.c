df60e4068ad5189d4554c9badd01445f23eb674c
gurukiran37 <gurunaik3975@gmail.com>
2025-02-18 23:10:55 +0530

    Update PROB2.c

--- DAY2/PROB2.c
+++ DAY2/PROB2.c
@@ -1,15 +1,49 @@
 #include <stdio.h>
-void main() 
+#include <stdlib.h>
+
+typedef struct {
+    char* name; 
+    int marks;
+} Student;
+
+int main() 
 {
-    int n,i,j;
-    printf("Enter the number of rows for the triangle: ");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++) 
+    int num, i;
+    printf("Enter number of students: ");
+    scanf("%d", &num);
+
+    Student *stud = (Student *)malloc(num * sizeof(Student));
+    if (stud == NULL) 
     {
-        for (j = 1; j <= i; j++) 
-        {
-            printf("* ");
-        }
-        printf("\n");
+        printf("Memory allocation failed\n");
+        return 1; 
     }
+
+    for (i = 0; i < num; i++) 
+    {
+        stud[i].name = (char *)malloc(100 * sizeof(char));
+        if (stud[i].name == NULL) 
+        {
+            printf("Memory allocation for name failed\n");
+            return 1; 
+        }
+        printf("Enter student %d name: ", i + 1);
+        scanf("%s", stud[i].name);
+        printf("Enter marks: ");
+        scanf("%d", &stud[i].marks);
+    }
+
+    printf("\nStudent Records:\n");
+    for (i = 0; i < num; i++) 
+    {
+        printf("Name:%s\t Marks:%d\n", stud[i].name, stud[i].marks);
+    }
+
+    for (i = 0; i < num; i++)
+    {
+        free(stud[i].name);
+    }
+    free(stud); 
+
+    return 0; 
 }
