51c5d02ce9b112b1a58aa221f6ace58d84230179
gurukiran37 <gurunaik3975@gmail.com>
2025-02-18 23:14:09 +0530

    Update PROB3.c

--- DAY2/PROB3.c
+++ DAY2/PROB3.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-int factorial(int n) 
+float calculate(float bill) 
 {
-    if (n == 0 || n == 1) 
+    float discount = 0.0;
+    if(bill>=500 && bill<=1000) 
     {
-        return 1;
-    } else {
-        return n * factorial(n - 1); 
+        discount = 0.10;
+    } else if(bill>1000) 
+    {
+        discount = 0.20;
     }
+    return bill*(1-discount); 
 }
 int main() 
 {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    if (num < 0) 
-    {
-        printf("Factorial is not defined for negative numbers.\n");
-    } 
-    else {
-     int result = factorial(num);
-     printf("Factorial of %d is %d\n", num,result);
-    }
-return 0;
+    float bill;
+    printf("Enter total bill amount:");
+    scanf("%f", &bill);
+    float final= calculate(bill);
+    printf("Final Payable Amount: ₹%.2f\n", final);
+    return 0;
 }
